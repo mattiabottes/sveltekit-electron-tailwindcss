53a9efa9c965cc7eaed4840e8a2fd52c86c562b2
mattiabottes <mattiabottes@gmail.com>
2023-06-01 15:41:18 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Sveltekit + Electron + Tailwind CSS
+
+Minimal [Sveltekit](https://github.com/sveltejs/kit#readme) + [Electron](https://www.electronjs.org/) and [Tailwind CSS](https://tailwindcss.com/) starter template.
+
+Super-inspired by Sveltekit + Electron by @FractalHQ: https://github.com/FractalHQ/sveltekit-electron
+
+<br />
+
+## Getting Started
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	let i = 0;
+</script>
+
+<main class="h-screen flex flex-col gap-4 items-center bg-blue-400 justify-center">
+	<h1 class="text-2xl font-semibold">SvelteKit + Electron + Tailwind CSS</h1>
+	<button
+		class="bg-white py-2 px-4 rounded-lg active:scale-95 transition:all"
+		on:click={() => i++}
+	>
+		Click me: <b>{i}</b>
+	</button>
+</main>
